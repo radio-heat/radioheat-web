6df361f796029a19b80fc5c9ee37aaefced12ade
Philip <Philip.Jurke@outlook.de>
2016-11-23 20:02:58 +0100

    Implement Google Maps engine

---
+++ main.js
@@ -0,0 +1,10 @@
+﻿
+var mapElement;
+
+function initMap()
+{
+    mapElement = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 51.3417825, lng: 12.3936349 },
+        zoom: 11
+    });
+}
